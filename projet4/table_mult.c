bb24359048e1691da11b754825598efe6a3c2236
Sarbatore <valentinchene69@gmail.com>
2022-10-01 14:47:52 +0200

    + de commentaires

--- projet4/table_mult.c
+++ projet4/table_mult.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 
-void mult_table(int nums)
-{
-  int max = 9; // Je calcule la table jusqu'à son multiple de 9
-
-  for (int i = 1; i <= nums; i++) // Faire les tables jusqu'à celle de "nums"
-  {
-    for (int y = 1; y <= max; y++) // Exécuter jusqu'au multiple "max"
-    {
-      printf("%d ", i*y); // Afficher i*le multiple "y"
-    }
-    printf("\n"); // Sauter une ligne à la fin de la table
-  }
-}
+// Prototypes //
+void mult_table(int nums);
 
 
+// Programme Principal //
 int main( )
 {
   int nums = 9; // Jusqu'à quelle table je vais ?
 
-  mult_table(nums); // Exécute les tables jusqu'à celle de nums
+  mult_table(nums); // Génère les tables jusqu'à celle de nums
 
   return 0;
+}
+
+
+// Fonctions //
+void mult_table(int nums) // Génère la table de multiplication jusqu'à celle de nums
+{
+  int max = 9; // Multiplier jusqu'à 9
+
+  for (int i = 1; i <= nums; i++)
+  {
+    for (int y = 1; y <= max; y++)
+    {
+      printf("%d ", i*y);
+    }
+    printf("\n");
+  }
 }
